7f45d676a4be868eeb82ca9bf448f84bf8bb63ac
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2018-12-12 00:07:09 -0500

    first commit

---
+++ 001-hello-world/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World!")
+}
+
+// control flow:
+// (1) sequence
+// (2) loop; iterative
+// (3) conditional
---
+++ 002-hello-world/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Start program")
+
+	foo()
+
+	fmt.Println("After foo")
+
+	bar()
+
+	fmt.Println("After bar")
+
+	gavin()
+
+	fmt.Println("End program!")
+}
+
+func foo() {
+
+	fmt.Println("This is foo!")
+}
+
+func bar() {
+
+	fmt.Println("This is bar!")
+
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func gavin() {
+
+	fmt.Println("This is gavin!")
+
+	for i := 0; i < 10; i++ {
+		if i%2 == 1 {
+			fmt.Println(i)
+		}
+	}
+}
