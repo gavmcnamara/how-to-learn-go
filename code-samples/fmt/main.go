e3f8d24ea5aae2195564d392690c6208711af661
Gavin McNamara <gavin.mcnamara01@myhunter.cuny.edu>
2018-12-12 00:47:31 -0500

    added comments to /fmt/main.go

--- code-samples/fmt/main.go
+++ code-samples/fmt/main.go
@@ -1,16 +1,27 @@
 package main
+// declaration of package
+// this code belogns in package main
 
-// Packages allows us to organize our code 
-// into differnt packages, similar to how 
+// Packages allows us to organize our code
+// into differnt packages, similar to how
 // folders are organized on our computers
 
 import (
 	"fmt"
 )
-
+// importing packages that you USE in your code
 func main() {
 
 	fmt.Println("Hello Everyone", 42, true)
+	// from package fmt im calling Println
+
+	n, err := fmt.Println("test", 13, false)
+	fmt.Println(n)
+	fmt.Println(err)
+	// catch any returns
+	// n = number of bytes written
+	// err = any errors
+	// NEVER HAVE A VARIABLE YOU DONT USE
 }
 
 // fmt.Println can pass in value of any type
